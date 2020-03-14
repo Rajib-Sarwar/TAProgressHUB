247101158f50c82e7c494a0d80c11cbbf6e19e94
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2020-03-14 20:01:46 +0530

    iteration one finished

---
+++ TAProgressHUB/TAProgressHUB/TAProgressHUB.h
@@ -0,0 +1,19 @@
+//
+//  TAProgressHUB.h
+//  TAProgressHUB
+//
+//  Created by Chowdhury Md Rajib  Sarwar on 14/3/20.
+//  Copyright © 2020 Chowdhury Md Rajib  Sarwar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TAProgressHUB.
+FOUNDATION_EXPORT double TAProgressHUBVersionNumber;
+
+//! Project version string for TAProgressHUB.
+FOUNDATION_EXPORT const unsigned char TAProgressHUBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TAProgressHUB/PublicHeader.h>
+
+
